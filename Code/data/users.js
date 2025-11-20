4f75da1f746fb6288a2dd8378b6a825dad5f1952
Amaan Rajguru <amaan.rajguru@gmail.com>
2025-11-20 13:49:13 -0500

    Method input added
    Added the required variables into methods

--- Code/data/users.js
+++ Code/data/users.js
@@ -3,25 +3,35 @@ import { dbConnection } from "../config/mongoConnection.js";
 import { ObjectId } from "mongodb";
 
 export const createUsers = async (
-
+username,
+email,
+password,
+profile,
+reviewIds,
+favoriteRestaurantIds
 ) => {
 
 }
 
 export const getbyUserID = async (
-
+userId
 ) => {
 
 }
 
 export const updateUser = async (
-
+username,
+email,
+password,
+profile,
+reviewIds,
+favoriteRestaurantIds
 ) => {
 
 }
 
 export const deleteUsers = async (
-
+username
 ) => {
 
 }

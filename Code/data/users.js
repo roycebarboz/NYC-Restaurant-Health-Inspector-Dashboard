b5a1e94ba73bb22be643f3eff43e6d279608e8c3
Amaan Rajguru <amaan.rajguru@gmail.com>
2025-12-01 18:29:30 -0500

    Registeruser 0.2 update
    Validated username & email address.
    
    Created a new helper function to check for duplicate usernames in the database.

--- Code/data/users.js
+++ Code/data/users.js
@@ -10,7 +10,19 @@ profile,
 reviewIds,
 favoriteRestaurantIds
 ) => {
+    if (!username||!email||!password||!profile){throw new Error("Register Error 101: Missing critical variables")};
     
+    if (typeof username != "string"|| username.trim().length == 0) {throw new Error("Register Error 201: Username has to be a valid non empty string")};
+    username = username.trim()
+    if (username.length < 2 || username.length > 20) {throw new Error("Register Error 202: Username has to be atleast 2 character's long and less than 20 characters")};
+
+    helperfunc.dupusercheck(username);
+
+    if (typeof email != "string" || email.trim().length==0){throw new Error("Register Error 301: email has to be a Non-Empty String");}
+    email = email.trim();
+    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){throw new Error("Register Error 302: email has to be valid email");}
+
+
 }
 
 export const getbyUserID = async (
--- Code/helpers.js
+++ Code/helpers.js
@@ -1,9 +1,17 @@
+import { dbConnection } from "./config/mongoConnection"
 import {ObjectID} from mongodb
 
-export const validation(varname, type) => {
-    if (!varname){ throw new Error(`Validation Err 100: ${varname} cannot be null`);}
-    if (!type){throw new Error(`Validation Err 101: ${type} cannot be null`);}
-    if (type == "string") {if (typeof varname != type||varname.trim().length===0){throw new Error(`Validation Err 201: ${type} cannot be empty string`)};}
-    if (type == "number") {if  (typeof varname != type||varname.trim().length===0){throw new Error(`Validation Err 202i: ${type} cannot be empty number`)};}
-    if (type == "ObjectID") {if  (typeof varname != type||varname.trim().length===0){throw new Error(`Validation Err 202i: ${type} cannot be empty number`)};}
+export const dupusercheck = async (username) => {
+    let dbConnection = await dbConnection();
+    let usersCollection = dbConnection.collection("users");
+
+    const usercheck = await usersCollection.findone({
+        username: {
+            $regex: `^${username}`,
+            $options: "i"
+        }
+    });
+    if(usercheck){throw new Error("DupCheck Error 100: Username is already registered");}
+
+    return true;
 }

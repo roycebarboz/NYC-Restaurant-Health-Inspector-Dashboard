8de0c6810048ac2d09dada54d3ee65cf0b62f91f
Amaan Rajguru <amaan.rajguru@gmail.com>
2025-12-02 14:18:24 -0500

    Set up Routing of login and signup
    Utalized Lab 10 logic to create base shell then set up system in handle bar to provide information for get. based on result reroute to login or error.
    
    login base hides register now based on if login has been completed

--- Code/routes/users.js
+++ Code/routes/users.js
@@ -1,7 +1,70 @@
 import {Router} from 'express';
+import { createUsers } from '../data/users';
 const router = Router();
 
+
+router.route('/').get(async (req, res) => {
+    try{
+    res.render('login',{title:"Login"});
+    }
+    catch(e){
+    res.status(500).send(e);
+    }
+});
+
 router
-    .route()
+    .route('/register')
+    .get(async (req, res) => {
+    try{
+    res.render('register',{title:"Register"});
+    }
+    catch(e){
+    res.status(500).send(e);
+    }
+    })
+    .post(async (req, res) => {
+    try{
+        const { username, email, password, profile} = req.body;
+        const result = await createUsers(username,email,password,profile);
+
+        res.render('login',{
+            title: "Login",
+            registrationCompleted: true
+        });
+    }
+    catch(e){
+        res.status(400).render('error',{
+            title:'Register',
+            error:e.message
+        });
+    }
+});
+
+router.route('/user').get(async (req, res) => {
+    try{
+    res.render('user',{title:"User"});
+    }
+    catch(e){
+    res.status(500).send(e);
+    }
+});
+
+router.route('/signout').get(async (req, res) => {
+    try{
+    res.render('signout',{title:"User"});
+    }
+    catch(e){
+    res.status(500).send(e);
+    }
+});
+
+router.route('/profile').get(async (req, res) => {
+    try{
+    res.render('[user_profile',{title:"Profile"});
+    }
+    catch(e){
+    res.status(500).send(e);
+    }
+});
 
 export default router;

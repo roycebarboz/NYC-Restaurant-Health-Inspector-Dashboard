b1553234e2d69cb633296a23d96bacb8aebef817
Amaan Rajguru <amaan.rajguru@gmail.com>
2025-12-01 19:40:41 -0500

    RegisterUser v0.3
    Set up password, profile validation, calculated createdAt and lastlogin values and have put a rough insert & insert checker
    
    Profile validation in helper function
    
    password still needs to be encrypted before insert

--- Code/data/users.js
+++ Code/data/users.js
@@ -6,10 +6,11 @@ export const createUsers = async (
 username,
 email,
 password,
-profile,
-reviewIds,
-favoriteRestaurantIds
+profile
 ) => {
+    const db = await dbConnection();
+    const usersCollection = db.collection("users");
+
     if (!username||!email||!password||!profile){throw new Error("Register Error 101: Missing critical variables")};
     
     if (typeof username != "string"|| username.trim().length == 0) {throw new Error("Register Error 201: Username has to be a valid non empty string")};
@@ -22,7 +23,37 @@ favoriteRestaurantIds
     email = email.trim();
     if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){throw new Error("Register Error 302: email has to be valid email");}
 
+    
+    if (typeof password != "string" || password.trim().length==0){throw new Error("Register Error 401: password has to be a Non-Empty String");}
+    password = password.trim();
+    if (username.length < 8 || username.length > 20) {throw new Error("Register Error 402: Password has to be atleast 8 character's long and less than 20 characters")};
+    if(!/[^A-Z][^0-9][^{p}]+$/.test(email)){throw new Error("Register Error 403: password has to have 1 Uppercase Letter, 1 digit and 1 special character");}
 
+    helperfunc.profilecheck(profile);
+
+    let reviewIds = [];
+    let favoriteRestaurantIds = [];
+
+    const createdAt = new Date();
+    const lastLogin = new Date();
+    lastLogin.setDate(0);
+
+    password = bcrypt(password);
+
+    const insertResult = await usersCollection.insertOne({
+        username,
+        email,
+        password,
+        profile,
+        reviewIds,
+        favoriteRestaurantIds,
+        createdAt,
+        lastLogin
+    });
+
+    const newUser = await usersCollection.findOne({_id:insertResult.insertedId});
+    if (!newUser) {throw new Error("Register Error 500: Failed to register User")}
+    else { return "{registrationCompleted: true}" };
 }
 
 export const getbyUserID = async (
--- Code/helpers.js
+++ Code/helpers.js
@@ -15,3 +15,23 @@ export const dupusercheck = async (username) => {
 
     return true;
 }
+
+export const profilecheck = async (profile) => {
+
+    const ValidKeys = ["firstName","lastName","dateOfBirth"];
+    const profilekeys = Object.keys(profile);
+
+    if (ValidKeys.length !== profilekeys.length||!ValidKeys.every(valid => profilekeys.includes(valid))){throw new Error("ProfileCheck 1 : Profile is missing keys or has too many keys");}
+    ValidKeys.forEach(keys =>{
+        if(typeof profile[keys] !== "string" || profile[keys].trim().length === 0){throw new Error(`ProfileCheck 2: ${keys} must be a non empty string`);}
+    });
+
+    const firstname = profile["firstName"];
+    if (!username||  typeof username != "string"|| username.trim().length == 0) {throw new Error("Register Error 201: Username has to be a valid non empty string")};
+    username = username.trim()
+    if (username.length < 2 || username.length > 20) {throw new Error("Register Error 202: Username has to be atleast 2 character's long and less than 20 characters")};
+
+    const lastname = profile["lastName"];
+
+    const dob = profile["dateOfBirth"];
+}
